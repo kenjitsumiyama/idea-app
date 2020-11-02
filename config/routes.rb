de795c59a2da195600d4c7767d6ae8dfe4a1be3a
kenjitsumiyama <kenjitsumiyama0724@gmail.com>
2020-11-02 11:22:44 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # get 'ideas/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "ideas#index"
+end
