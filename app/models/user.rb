59506e19b8d8996e12de970e4668f0acebb94e44
kenjitsumiyama <kenjitsumiyama0724@gmail.com>
2020-11-02 17:43:05 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :ideas
+  has_many :comments
+  
+  with_options presence: true do
+  validates :name
+  end
 end
