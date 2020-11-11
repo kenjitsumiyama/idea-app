cc4c887c49bd3aee5b65002c3f08d2994d0e2fce
kenjitsumiyama <kenjitsumiyama0724@gmail.com>
2020-11-11 18:21:58 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @ideas = @user.ideas.order(id: 'DESC')
+    # @comments = @idea.comments.includes(:user).order(id: 'DESC')
   end
 end
